4d3fb3b0ba3bbc197e5f48e53dabf429ddd6a950
wajid <wajidsheikh443@gmail.com>
2026-04-27 15:14:21 +0500

    final fix for path-to-regexp using regex

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,16 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
-
 app.use("/api/products", productRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("(.*)", (req, res) => {
+  app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
+
 const startServer = async () => {
   try {
     await connectDB(); 
